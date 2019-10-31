aaccc182b0b220a197a38a862c5a98e149434db9
lqs <932044860@qq.com>
2019-10-31 10:32:56 +0800

    IconTopButton component

---
+++ components/IconTopButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="icon-top-button">
+    <i :class="icon"></i>
+    <span>{{ name }}</span>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String
+    },
+    icon: {
+      type: String,
+      default: 'el-icon-close'
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+$font-family: 'element-icons', 'Tahoma', 'Microsoft YaHei';
+$button-text-size: 12px;
+$button-icon-size: 24px;
+$active-color: #ff4526;
+.icon-top-button {
+  background-color: #fff;
+  border: 1px rgb(220, 223, 230) solid;
+  border-radius: 10px;
+  display: block;
+  padding: 6px 15px;
+  margin: 0 8px 8px 8px;
+  outline: none;
+  text-align: center;
+  cursor: pointer;
+  font: $button-icon-size $font-family !important;
+  &:hover {
+      color: $active-color;
+  }
+  & span {
+    font-size: $button-text-size;
+    display: block;
+  }
+}
+</style>
